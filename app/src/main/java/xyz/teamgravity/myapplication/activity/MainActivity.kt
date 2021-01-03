1d94ddbdac02202cf6a0ba3a499db1566e4ef030
Raheem <raheemideadev@gmail.com>
2021-01-03 15:59:42 +0500

    raheem

--- app/src/main/java/xyz/teamgravity/myapplication/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/myapplication/activity/MainActivity.kt
@@ -2,16 +2,25 @@ package xyz.teamgravity.myapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import xyz.teamgravity.myapplication.R
 import xyz.teamgravity.myapplication.databinding.ActivityMainBinding
+import xyz.teamgravity.myapplication.viewmodel.RunDao
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var dao: RunDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        println("debug: ${dao.hashCode()}")
     }
 }
