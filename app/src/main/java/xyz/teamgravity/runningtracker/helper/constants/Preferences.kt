7aec9ade299df0745a6b14fc6778270705019173
Raheem <raheemideadev@gmail.com>
2021-01-05 16:41:45 +0500

    raheem

---
+++ app/src/main/java/xyz/teamgravity/runningtracker/helper/constants/Preferences.kt
@@ -0,0 +1,14 @@
+package xyz.teamgravity.runningtracker.helper.constants
+
+object Preferences {
+
+    /**
+     * SharedPreferences name
+     */
+    const val NAME = "prefs"
+
+    /**
+     * It is used to determine if user first time opening app, if so get some personal data
+     */
+    const val IS_SET_UP = "isSetUp"
+}
--- app/src/main/java/xyz/teamgravity/runningtracker/injection/DatabaseModule.kt
+++ app/src/main/java/xyz/teamgravity/runningtracker/injection/DatabaseModule.kt
@@ -7,6 +7,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import xyz.teamgravity.runningtracker.helper.constants.Preferences
 import xyz.teamgravity.runningtracker.viewmodel.MyDatabase
 import javax.inject.Singleton
 
@@ -16,10 +17,15 @@ object DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideMyDatabase(@ApplicationContext context: Context)
-    = Room.databaseBuilder(context.applicationContext, MyDatabase::class.java, MyDatabase.DATABASE_NAME).build()
+    fun provideMyDatabase(@ApplicationContext app: Context)
+    = Room.databaseBuilder(app.applicationContext, MyDatabase::class.java, MyDatabase.DATABASE_NAME).build()
 
     @Singleton
     @Provides
     fun provideRunDao(db: MyDatabase) = db.runDao()
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(Preferences.NAME, Context.MODE_PRIVATE)
 }
